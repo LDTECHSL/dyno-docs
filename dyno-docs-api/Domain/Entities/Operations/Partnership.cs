8e04ab36adcf078448f0b5f53729306c673002ff
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-01-27 19:25:08 +0530

    feat: Implement CRUD operations for Partnerships
    - Added DeletePartnershipCommand and its handler for deleting partnerships.
    - Introduced UpdatePartnershipCommand and handler for updating partnership details.
    - Created GetPartnerships query and handler to retrieve all partnerships.
    - Defined PartnershipTypes enum for categorizing partnerships.
    - Implemented Partnership entity with relevant properties.
    - Added database migration for creating the Partnership table.
    - Created API endpoints in PartnershipController for managing partnerships.
    - Developed Partnerships page in the UI for creating, updating, deleting, and listing partnerships.
    - Integrated partnership API service for handling API requests related to partnerships.
    - Enhanced styles for the Partnerships page layout.

---
+++ dyno-docs-api/Domain/Entities/Operations/Partnership.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Common;
+
+namespace Domain.Entities.Operations;
+
+public class Partnership : BaseEntity
+{
+    [MaxLength(255)]
+    public string? Name { get; set; }
+    [MaxLength(500)]
+    public string? Description { get; set; }
+    [MaxLength(225)]
+    public string? District { get; set; }
+    public PartnershipTypes PartnershipType { get; set; }
+    public byte[][]? Images { get; set; }
+}
