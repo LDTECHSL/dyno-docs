b6cc844ae3f91126cb390ec0c93f38ef10c437dd
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-01-31 21:11:37 +0530

    feat: add template management functionality
    - Created migration to update TemplateDesign column type in the database.
    - Implemented TemplateController for handling template creation and retrieval.
    - Developed Templates page in the UI to display and manage templates.
    - Added API service for fetching templates from the backend.
    - Styled templates page and components for better user experience.

---
+++ dyno-docs-api/Domain/Entities/Operations/Template.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Common;
+
+namespace Domain.Entities.Operations;
+
+public class Template : BaseEntity
+{
+    [Required]
+    public string TemplateName { get; set; }
+    
+    [Required]
+    public byte[] TemplateThumbnail { get; set; }
+    public string TemplateDesign { get; set; } = string.Empty;
+    public bool isPaid { get; set; }
+    public decimal? Price { get; set; }
+}
